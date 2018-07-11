e53e7d4f489cf7b6a0c4c50fde4879e2d61ceeb2
sam6321 <sam6321@live.com.au>
2018-07-11 11:50:00 +0800

    Added downsampling program. Attempting to run plots on pleiades

--- src/plots.py
+++ src/plots.py
@@ -42,106 +42,6 @@ SAMPLE_RATE = 32000000
 # X = samples, Y = frequency band 0 to 4, Z = P0 or P1
 # mlgpu, 14000M
 
-def fft_freq_sort(samples):
-    """
-    Uses cp.fft.fftfreq to generate sample frequencies.
-    Frequencies and samples are sorted from lowest to highest frequency to ensure
-    results are plotted correctly.
-    :param samples:
-    :return:
-    """
-    freq_sample = cp.fft.fftfreq(samples.shape[0], d=32000000)
-    sort_indices = cp.argsort(freq_sample)
-    return freq_sample[sort_indices], samples[sort_indices]
-
-
-def fft_linspace(samples):
-    """
-    Generates sample 'frequencies' starting at 0, going to num_samples / 2
-    This should show half the fft result
-    :param samples:
-    :return:
-    """
-    freq_sample = cp.linspace(0, 1.0 / (2.0 * 32000000), samples.shape[0] // 2)
-    return freq_sample, samples[0:samples.shape[0] // 2]
-
-
-def fft_sum_amplitudes(samples):
-    """
-    Calculates the amplitude abs(ft) for all frequencies, and sums them, returning
-    the summed amplitudes
-    :param samples:
-    :return:
-    """
-    # Entire file contains 1920000000 samples over 1 minute
-    # 32000000 samples per second
-    # Fourier transform and sum amplitudes
-    # fftfreq provides x axis values, amplitude is y axis values
-    amps = None #cp.zeros(samples.shape[0])
-    for freq in range(samples.shape[1]):
-        ft = cp.abs(cp.fft.rfft(samples[:, freq]))
-        if amps is None:
-            amps = ft
-        else :
-            amps += ft
-
-    return amps
-
-
-def fft_sum_amplitudes2(samples):
-    """
-    Sums amplitudes in a different way, including some sort of normalisation.
-    I found it here and thought I'd experiment:
-    https://docs.scipy.org/doc/scipy-1.1.0/reference/tutorial/fftpack.html#one-dimensional-discrete-fourier-transforms
-    :param samples:
-    :return:
-    """
-    amps = []
-    for freq in range(samples.shape[1]):
-        ft = cp.fft.fft(samples[:, freq])
-        amps.append(2.0 / samples.shape[0] * cp.abs(ft))
-    return cp.sum(amps, axis=0)
-
-
-def fft_power_spectrum(samples):
-    """
-    Pretty sure this calculates the power spectrum. That's what the equation at
-    https://www.wavemetrics.com/products/igorpro/dataanalysis/signalprocessing/powerspectra.htm
-    seems to say.
-    Also sums it up but I'm not sure if I'm meant to do that
-    :param samples:
-    :return:
-    """
-    sum = []
-    for freq in range(samples.shape[1]):
-        ft = cp.fft.fft(samples[:, freq])
-        sum.append((cp.abs(ft)**2) / samples.shape[0])
-
-    return cp.sum(sum, axis=0)
-
-
-def plot_fft(polarisation, filename, pindex):
-    x, y = fft_freq_sort(fft_sum_amplitudes(polarisation))
-    plt.title("P{0} {1} fft".format(pindex, filename))
-    plt.xlabel("Frequency")
-    plt.ylabel("Amplitude")
-
-    #x, y = fft_linspace(fft_sum_amplitudes2(polarisation))
-    plt.plot(cp.asnumpy(x), cp.asnumpy(y), label="p{0}".format(pindex), lw=0.5)
-    plt.legend()
-    plt.show()
-
-
-def plot_over_time(polarisation, filename, pindex):
-    for freq in range(polarisation.shape[1]):
-        y = polarisation[:, freq]
-        x = cp.linspace(0, y.shape[0] / 32000000.0, y.shape[0])
-        plt.title("P{0} {1} over time".format(pindex, filename))
-        plt.xlabel("Time"),
-        plt.ylabel("Power")
-        plt.plot(x, y)
-        plt.show()
-
 
 class LBAPlotter(object):
     def __init__(self, filename, out_directory, sample_offset=0, num_samples=0):
@@ -171,7 +71,9 @@ class LBAPlotter(object):
 
     @staticmethod
     def fix_freq(f, freq_index):
-        f /= 1e6
+        f -= np.min(f)
+        f /= np.max(f)
+        f *= 16.0
         f += 6700.0 + 16.0 * freq_index
 
     @staticmethod
@@ -218,15 +120,18 @@ class LBAPlotter(object):
 
     def output_sample_statistics(self, samples):
         # Sample statistics
-        ss = self.create_sample_statistics(samples)
-        with open(self.get_output_filename("sample_statistics.txt"), "w") as sf:
-            json.dump(ss, sf, indent=4)
-        # histogram of sample statistics
-        self.save_sample_statistics_histogram(ss)
+        try:
+            ss = self.create_sample_statistics(samples)
+            with open(self.get_output_filename("sample_statistics.json"), "w") as sf:
+                json.dump(ss, sf, indent=4)
+            # histogram of sample statistics
+            self.save_sample_statistics_histogram(ss)
+        except Exception as e:
+            print("Error ouputting sample statistics {0}".format(e))
 
     @staticmethod
     def create_spectrogram(freq):
-        return signal.spectrogram(freq, fs=SAMPLE_RATE)
+        return signal.spectrogram(freq, fs=SAMPLE_RATE, window=('tukey', 0.5))
 
     def merge_spectrograms(self, spectrograms, normalise_local=False):
         fs = []
@@ -257,7 +162,7 @@ class LBAPlotter(object):
 
     @staticmethod
     def create_periodogram(freq):
-        return signal.periodogram(freq, fs=SAMPLE_RATE)
+        return signal.periodogram(freq, fs=SAMPLE_RATE, window=('tukey', 0.5))
 
     def save_periodogram(self, periodogram):
         f, pxx = periodogram
@@ -272,7 +177,7 @@ class LBAPlotter(object):
 
     @staticmethod
     def create_welch(freq):
-        return signal.welch(freq, fs=SAMPLE_RATE)
+        return signal.welch(freq, fs=SAMPLE_RATE, window=('tukey', 0.5))
 
     def save_welch(self, welch):
         f, spec = welch
@@ -308,7 +213,7 @@ class LBAPlotter(object):
         plt.close(fig)
 
     def create_fft(self, samples):
-        ft = np.fft.rfft(samples)
+        ft = np.fft.rfft(samples * signal.windows.tukey(samples.shape[0], sym=False))
         f = np.fft.rfftfreq(samples.shape[0], d=SAMPLE_RATE)
         np.abs(ft, ft)
         return f, ft
@@ -325,7 +230,7 @@ class LBAPlotter(object):
         plt.close(fig)
 
     def create_psd(self, samples):
-        return mlab.psd(samples, Fs=SAMPLE_RATE)
+        return mlab.psd(samples, Fs=SAMPLE_RATE, window=signal.get_window(('tukey', 0.5), 256))
 
     def save_psd_asd(self, psd, type):
         Pxx, freqs = psd
@@ -342,87 +247,94 @@ class LBAPlotter(object):
         # Firstly, create the output directory
         os.makedirs(self.out_directory, exist_ok=True)
 
-        with open(self.filename, "r") as f:
-            lba = LBAFile(f)
-            samples = lba.read(self.sample_offset, self.num_samples)
+        if self.filename.endswith(".lba"):
+            with open(self.filename, "r") as f:
+                lba = LBAFile(f)
+                samples = lba.read(self.sample_offset, self.num_samples)
+        elif self.filename.endswith(".npz"):
+            samples = np.load(self.filename)["arr_0"]
 
-            # Do global things across all samples
-            self.output_sample_statistics(samples)
+        # Do global things across all samples
+        self.output_sample_statistics(samples)
 
-            # Split into p0 and p1
-            p0 = samples[:, :, 0]
-            p1 = samples[:, :, 1]
-            for pindex, p in enumerate((p0, p1)):
-                # Do things for each polarisation
-                self.polarisation = pindex
+        # Split into p0 and p1
+        p0 = samples[:, :, 0]
+        p1 = samples[:, :, 1]
+        for pindex, p in enumerate((p0, p1)):
+            # Do things for each polarisation
+            self.polarisation = pindex
+            os.makedirs(self.get_output_filename(), exist_ok=True)
+
+            self.output_sample_statistics(p)
+
+            spectrograms = []
+
+            for freq in range(p.shape[1]):
+                # Do things for each frequency
+                freq_samples = p[:, freq]
+
+                self.frequency = freq
                 os.makedirs(self.get_output_filename(), exist_ok=True)
 
-                self.output_sample_statistics(p)
+                self.output_sample_statistics(freq_samples)
 
-                spectrograms = []
+                # Spectrogram for this frequency
+                f, t, sxx = self.create_spectrogram(freq_samples)
+                # Calculate the actual frequencies for the spectrogram
+                self.fix_freq(f, freq)
+                spectrogram = (f, t, sxx)
+                spectrograms.append(spectrogram)
+                self.save_spectrogram(spectrogram)
 
-                for freq in range(p.shape[1]):
-                    # Do things for each frequency
-                    freq_samples = p[:, freq]
+                # Periodogram for this frequency
+                f, pxx = self.create_periodogram(freq_samples)
+                self.fix_freq(f, freq)
+                self.save_periodogram((f, pxx))
 
-                    self.frequency = freq
-                    os.makedirs(self.get_output_filename(), exist_ok=True)
+                # Welch
+                f, spec = self.create_welch(freq_samples)
+                self.fix_freq(f, freq)
+                self.save_welch((f, spec))
 
-                    self.output_sample_statistics(freq_samples)
-
-                    # Spectrogram for this frequency
-                    f, t, sxx = self.create_spectrogram(freq_samples)
-                    # Calculate the actual frequencies for the spectrogram
-                    self.fix_freq(f, freq)
-                    spectrogram = (f, t, sxx)
-                    spectrograms.append(spectrogram)
-                    self.save_spectrogram(spectrogram)
-
-                    # Periodogram for this frequency
-                    f, pxx = self.create_periodogram(freq_samples)
-                    self.fix_freq(f, freq)
-                    self.save_periodogram((f, pxx))
-
-                    # Welch
-                    f, spec = self.create_welch(freq_samples)
-                    self.fix_freq(f, freq)
-                    self.save_welch((f, spec))
-
-                    # Lombscargle
+                # Lombscargle
+                try:
                     f, pxx = self.create_lombscargle(freq_samples)
                     self.fix_freq(f, freq)
                     self.save_lombscargle((f, pxx))
+                except ZeroDivisionError:
+                    print("Zero division in Lombscargle")
 
-                    # FFT
-                    f, ft = self.create_fft(freq_samples)
-                    self.fix_freq(f, freq)
-                    self.save_fft((f, ft))
+                # FFT
+                f, ft = self.create_fft(freq_samples)
+                self.fix_freq(f, freq)
+                self.save_fft((f, ft))
 
-                    # power spectral density
-                    Pxx, f = self.create_psd(freq_samples)
-                    self.fix_freq(f, freq)
-                    self.save_psd_asd((Pxx, f), "psd")
+                # power spectral density
+                Pxx, f = self.create_psd(freq_samples)
+                self.fix_freq(f, freq)
+                self.save_psd_asd((Pxx, f), "psd")
 
-                    # amplitude spectral density
-                    np.sqrt(Pxx, Pxx)
-                    self.save_psd_asd((Pxx, f), "asd")
+                # amplitude spectral density
+                np.sqrt(Pxx, Pxx)
+                self.save_psd_asd((Pxx, f), "asd")
 
-                self.frequency = None
+            self.frequency = None
 
-                # Create merged spectrograms for this p
-                merged = self.merge_spectrograms(spectrograms)
-                merged_normalised = self.merge_spectrograms(spectrograms, normalise_local=True)
-                self.save_spectrogram(merged, "merged", "spectrogram_merged.png")
-                self.save_spectrogram(merged_normalised, "merged local normalisation", "spectrogram_merged_normalised.png")
+            # Create merged spectrograms for this p
+            merged = self.merge_spectrograms(spectrograms)
+            merged_normalised = self.merge_spectrograms(spectrograms, normalise_local=True)
+            self.save_spectrogram(merged, "merged", "spectrogram_merged.png")
+            self.save_spectrogram(merged_normalised, "merged local normalisation", "spectrogram_merged_normalised.png")
 
 
 if __name__ == "__main__":
     queue = JobQueue(8)
 
     # Load each file using a process pool
-    num_samples = 102400
+    num_samples = SAMPLE_RATE # should be 1 second
     queue.submit(LBAPlotter("../data/v255ae_At_072_060000.lba", "./At_out/", num_samples=num_samples))
     queue.submit(LBAPlotter("../data/v255ae_Mp_072_060000.lba", "./Mp_out/", num_samples=num_samples))
     queue.submit(LBAPlotter("../data/vt255ae_Pa_072_060000.lba", "./Pa_out/", num_samples=num_samples))
+    #queue.submit(LBAPlotter("../data/downsamples.npz", "./downsamples_out/"))
 
     queue.join()
