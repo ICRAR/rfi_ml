6db99042421aec33321a4be8be04b7547eba28ac
ittpmichael <ittpmichael@github.com>
2018-11-05 11:53:20 +0800

    add argument parser and auto GPU/CPU selector

--- src/gan/train.py
+++ src/gan/train.py
@@ -23,30 +23,63 @@
 
 import sys
 import os
-base_path = os.path.dirname(__file__)
-sys.path.append(os.path.abspath(os.path.join(base_path, '..')))
-
+import argparse
 import logging
 from datetime import datetime
-from torch import nn, optim, Tensor, version
+from torch import nn, optim, Tensor, version, cuda
 from gan import USE_CUDA
 from gan.checkpoint import Checkpoint
 from gan.data import Data
 from gan.models.single_polarisation_single_frequency import Generator, Discriminator
 from gan.visualise import Visualiser
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+base_path = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(os.path.join(base_path, '..')))
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 LOG = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train GAN model")
+    parser.add_argument('--autoencoder', default=True,
+                        help="If True, enable the autoencoder")
+    parser.add_argument('-b, --batch', default=2048, type=int,
+                        help="batch size used for training GAN model")
+    parser.add_argument('--fft', default=True,
+                        help="Perform an FFT on the data and save the FFT output")
+    parser.add_argument('--samples', type=int, default=1000000000 // 2,
+                        help="Number of samples to read. If using --fft, this will round to the next multiple of --fft_size")  # can't load 16gb of data in you fool
+    parser.add_argument('--width', type=int, default=2048,
+                        help="Width size for Discriminator and Generator")
+    return vars(parser.parse_args())
+
 
 SAMPLE_SIZE = 1024  # 1024 signal samples to train on
 TRAINING_BATCH_SIZE = 100
 TRAINING_BATCHES = 10000
 
 USE_ANGLE_ABS = False  # Convert from real, imag input to abs, angle
-ADD_DROPOUT = True  # if true, add dropout to the inputs before passing them into the network
+# if true, add dropout to the inputs before passing them into the network
+ADD_DROPOUT = True
 ADD_NOISE = False  # if true, add noise to the inputs before passing them into the network
 
-print(version.cuda)
+print("Searching for CUDA device...")
+num_device = cuda.device_count()
+if num_device > 0:
+    USE_CUDA = True
+    print("Found", num_device, "CUDA device(s)")
+    print("Checking capability...")
+    if cuda.get_device_capability(0)[0] > 3:
+        USE_CUDA = True
+        print("Passes, CUDA enabled with version: ", version.cuda)
+    else:
+        USE_CUDA = False
+        print("Device is too old, switched to CPU")
+else:
+    print("No CUDA device found, use CPU")
+    USE_CUDA = False
 
 
 class Train(object):
@@ -78,13 +111,15 @@ class Train(object):
             self.generator = self._generator
 
         # self.discriminator_optimiser = optim.Adam(self.discriminator.parameters(), lr=0.0003, betas=(0.5, 0.999))
-        self.generator_optimiser = optim.Adam(self.generator.parameters(), lr=0.005, betas=(0.5, 0.999))
+        self.generator_optimiser = optim.Adam(
+            self.generator.parameters(), lr=0.005, betas=(0.5, 0.999))
 
     def train_discriminator_autoencoder(self, filename, max_epochs):
         LOG.info("Training generator as autoencoder...")
 
         try:
-            epoch = Checkpoint.try_restore("generator", self.generator, self.generator_optimiser)
+            epoch = Checkpoint.try_restore(
+                "generator", self.generator, self.generator_optimiser)
             if epoch is None:
                 epoch = 0
                 LOG.info("No generator checkpoint to restore")
@@ -99,14 +134,17 @@ class Train(object):
 
         LOG.info("Training complete, saving final model state")
         Checkpoint.create_directory("generator_complete")
-        Checkpoint.save_state("generator_complete", self.generator.state_dict(), self.generator_optimiser.state_dict(), -1)
+        Checkpoint.save_state("generator_complete", self.generator.state_dict(
+        ), self.generator_optimiser.state_dict(), -1)
 
     def _train_discriminator_autoencoder(self, filename, start_epoch, max_epochs):
         criterion = nn.SmoothL1Loss()
 
         LOG.info("Loading data...")
-        data_loader = Data(filename, self.samples, self.width, self.noise_width, self.batch_size, use_angle_abs=USE_ANGLE_ABS)
-        vis = Visualiser(os.path.join(os.path.splitext(filename)[0], str(datetime.now())))
+        data_loader = Data(filename, self.samples, self.width,
+                           self.noise_width, self.batch_size, use_angle_abs=USE_ANGLE_ABS)
+        vis = Visualiser(os.path.join(
+            os.path.splitext(filename)[0], str(datetime.now())))
         epoch = start_epoch
 
         while epoch < max_epochs:
@@ -129,7 +167,8 @@ class Train(object):
                     fmt = "Epoch [{0}/{1}], Step[{2}], loss: {3:.4f}"
                     LOG.info(fmt.format(epoch + 1, max_epochs, step, loss))
 
-            Checkpoint.save_state("generator", self.generator.state_dict(), self.generator_optimiser.state_dict(), epoch)
+            Checkpoint.save_state("generator", self.generator.state_dict(
+            ), self.generator_optimiser.state_dict(), epoch)
             vis.plot_training(epoch)
             data, _, _ = iter(data_loader).__next__()
             vis.test_autoencoder(epoch, self.generator, data_cuda)
@@ -141,15 +180,19 @@ class Train(object):
 
         epoch = 0
         try:
-            discriminator_epoch = Checkpoint.try_restore("discriminator", self.discriminator, self.discriminator_optimiser)
-            generator_epoch = Checkpoint.try_restore("generator", self.generator, self.generator_optimiser)
+            discriminator_epoch = Checkpoint.try_restore(
+                "discriminator", self.discriminator, self.discriminator_optimiser)
+            generator_epoch = Checkpoint.try_restore(
+                "generator", self.generator, self.generator_optimiser)
             if discriminator_epoch != generator_epoch:
-                LOG.error("Discriminator and generator checkpoints out of sync. Ignoring")
+                LOG.error(
+                    "Discriminator and generator checkpoints out of sync. Ignoring")
             elif discriminator_epoch is not None:
                 epoch = discriminator_epoch
             LOG.info("Restored checkpoint at epoch {0}".format(epoch))
         except Exception as e:
-            LOG.error("Failed to restore discriminator and generator {0}".format(e))
+            LOG.error(
+                "Failed to restore discriminator and generator {0}".format(e))
 
         self._generator.set_autoencoder(False)
         self._train(filename, epoch, max_epochs)
@@ -158,8 +201,10 @@ class Train(object):
         LOG.info("Training complete, saving final model state")
         Checkpoint.create_directory("discriminator_complete")
         Checkpoint.create_directory("generator_complete")
-        Checkpoint.save_state("discriminator_complete", self.discriminator.state_dict(), self.discriminator_optimiser.state_dict(), -1)
-        Checkpoint.save_state("generator_complete", self.generator.state_dict(), self.generator_optimiser.state_dict(), -1)
+        Checkpoint.save_state("discriminator_complete", self.discriminator.state_dict(
+        ), self.discriminator_optimiser.state_dict(), -1)
+        Checkpoint.save_state("generator_complete", self.generator.state_dict(
+        ), self.generator_optimiser.state_dict(), -1)
 
     def _train(self, filename, start_epoch, max_epochs):
         real_labels = None
@@ -169,8 +214,10 @@ class Train(object):
         epoch = start_epoch
 
         LOG.info("Loading data...")
-        data_loader = Data(filename, self.samples, self.width, self.noise_width, self.batch_size)
-        vis = Visualiser(os.path.join(os.path.splitext(filename)[0], str(datetime.now())))
+        data_loader = Data(filename, self.samples, self.width,
+                           self.noise_width, self.batch_size)
+        vis = Visualiser(os.path.join(
+            os.path.splitext(filename)[0], str(datetime.now())))
 
         # Training loop
         LOG.info("Training start")
@@ -178,9 +225,11 @@ class Train(object):
             for step, (data, noise1, noise2) in enumerate(data_loader):
                 batch_size = data.size(0)
                 if real_labels is None or real_labels.size(0) != batch_size:
-                    real_labels = data_loader.generate_labels(batch_size, [1.0, 0.0])
+                    real_labels = data_loader.generate_labels(
+                        batch_size, [1.0, 0.0])
                 if fake_labels is None or fake_labels.size(0) != batch_size:
-                    fake_labels = data_loader.generate_labels(batch_size, [0.0, 1.0])
+                    fake_labels = data_loader.generate_labels(
+                        batch_size, [0.0, 1.0])
 
                 # todo: Track training performance over time. Show random sample of input + what the generator created.
 
@@ -191,8 +240,10 @@ class Train(object):
                 d_output_fake1 = self.discriminator(self.generator(noise1))
 
                 # Determine the loss of the discriminator by adding up the real and fake loss and backpropagate
-                d_loss_real = criterion(d_output_real, real_labels)  # How good the discriminator is on real input
-                d_loss_fake = criterion(d_output_fake1, fake_labels)  # How good the discriminator is on fake input
+                # How good the discriminator is on real input
+                d_loss_real = criterion(d_output_real, real_labels)
+                # How good the discriminator is on fake input
+                d_loss_fake = criterion(d_output_fake1, fake_labels)
                 d_loss = 0.5 * (d_loss_real + d_loss_fake)
                 self.discriminator.zero_grad()
                 d_loss.backward()
@@ -215,11 +266,14 @@ class Train(object):
                 if step % 5 == 0:
                     # Report data and save checkpoint
                     fmt = "Epoch [{0}/{1}], Step[{2}], d_loss_real: {3:.4f}, d_loss_fake: {4:.4f}, g_loss: {5:.4f}"
-                    LOG.info(fmt.format(epoch + 1, max_epochs, step, d_loss_real, d_loss_fake, g_loss))
+                    LOG.info(fmt.format(epoch + 1, max_epochs, step,
+                                        d_loss_real, d_loss_fake, g_loss))
                     # self.plots.generate(real, g_output_fake1, g_output_fake2, epoch)
 
-            Checkpoint.save_state("discriminator", self.discriminator.state_dict(), self.discriminator_optimiser.state_dict(), epoch)
-            Checkpoint.save_state("generator", self.generator.state_dict(), self.generator_optimiser.state_dict(), epoch)
+            Checkpoint.save_state("discriminator", self.discriminator.state_dict(
+            ), self.discriminator_optimiser.state_dict(), epoch)
+            Checkpoint.save_state("generator", self.generator.state_dict(
+            ), self.generator_optimiser.state_dict(), epoch)
             vis.plot_training(epoch)
 
             data, noise1, noise2 = iter(data_loader).__next__()
@@ -228,18 +282,19 @@ class Train(object):
 
 
 if __name__ == "__main__":
-    autoencoder = True
-    samples = 1000000000 // 2  # can't load 16gb of data in you fool
-    width = 2048
-    fft = True
+    args = parse_args()
+
+    autoencoder, samples = args['autoencoder'], args['samples']
+    width, fft, batch = args['width'], args['fft'], args['batch']
     if fft:
         # 1000001536, next a largest multiple of width
         samples = samples - (samples % width) + width
         width *= 2  # Double width for fft (real + imag values)
 
-    train = Train(samples // width, width, 4096, fft)
+    train = Train(samples // width, width, batch, fft)
     if autoencoder:
         #train.train_discriminator_autoencoder("../../data/At_c0p0_c0_p0_s1000000000_fft4096.hdf5", 50)
-        train.train_discriminator_autoencoder("../../data/At_c0_p0_s1000000000_fft2048.hdf5", 50)
+        train.train_discriminator_autoencoder(
+            "../../data/At_c0_p0_s1000000000_fft2048.hdf5", 50)
     else:
         train.train("../../data/At_c0p0_c0_p0_s1000000000_fft4096.hdf5", 50)
